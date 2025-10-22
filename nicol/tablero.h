03e3bbda3d270d958b8d809995a43adc04dc5395
nicol2023 <144962704+nicol2023@users.noreply.github.com>
2025-10-22 15:11:17 -0300

    Add files via upload

---
+++ nicol/tablero.h
@@ -0,0 +1,12 @@
+#ifndef TABLERO_H
+#define TABLERO_H
+
+// Declaracion de la variable global del tablero
+extern int tablero[9][9];
+
+//declaraciones de funciones 
+void imprimirTablero();
+void inicializarTableroVacio();
+void probarTableroEjemplo();
+
+#endif
