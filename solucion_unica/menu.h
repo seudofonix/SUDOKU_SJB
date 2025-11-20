b9d720f65ae15a0f0fd7d02e422a5f258518bd54
nicol2023 <144962704+nicol2023@users.noreply.github.com>
2025-11-20 00:14:23 -0300

    Update menu.h

--- solucion_unica/menu.h
+++ solucion_unica/menu.h
@@ -7,18 +7,27 @@
 // Estados del juego
 typedef enum {
     ESTADO_MENU,
+    ESTADO_INGRESO_NOMBRE,
     ESTADO_JUGANDO,
+    ESTADO_RANKING,
     ESTADO_INSTRUCCIONES,
     ESTADO_SALIR
 } EstadoJuego;
 
 // Variable global del estado (se define en menu.c)
 extern EstadoJuego estadoActual;
+extern char nombreJugador[21];
 
-// Funciones del menú
+extern char ultimoJugador[21];
+extern int ultimoPuntaje;
+
+// Funciones del menÃº
 void dibujarMenuPrincipal(SDL_Renderer* renderer);
 void manejarClickMenu(int x, int y);
+void dibujarIngresoNombre(SDL_Renderer* renderer);
+void manejarEscrituraNombre(SDL_Event* event); // Ahora inicia el juego
+void dibujarRanking(SDL_Renderer* renderer);
+void manejarClickRanking(int x, int y);
 void dibujarInstrucciones(SDL_Renderer* renderer);
 void manejarClickInstrucciones(int x, int y);
-
 #endif
